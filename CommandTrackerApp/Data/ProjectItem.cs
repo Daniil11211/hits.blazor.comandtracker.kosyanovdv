d0d5a4eb44eaf97a58285805c9077c05f0c97512
Daniil Kosyanov <kosyanov.daniil@inbox.ru>
2025-12-04 01:52:12 +0500

    feature: добавление класса для проекта, создание дизайна для карточки проекта

---
+++ CommandTrackerApp/Data/ProjectItem.cs
@@ -0,0 +1,19 @@
+﻿namespace CommandTrackerApp.Data
+{
+    public class ProjectItem
+    {
+        public int Id { get; set; }
+        
+        public string Title { get; set; } = "Без названия";
+
+        public string Description { get; set; } = "Отстутсвует";
+
+        public DateTime? CreatedDate { get; set; }
+
+        public int CountMembers { get; set; }
+
+        public int Status { get; set; } = 1;
+
+        public int PrivateStatus { get; set; } = 1;
+    }
+}
